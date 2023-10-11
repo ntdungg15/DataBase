e20714d065272e531203f20deca1801fd60b016d
ntdungg15 <ngtiendung0105204@gmail.com>
2023-10-11 22:25:36 +0700

    update bth5

--- BTH/BTH_5.sql
+++ BTH/BTH_5.sql
@@ -1,5 +1,24 @@
 select substring(productDescription,1,50) AS 'title of products'
-from products
+from products;
+
+select CONCAT(lastName,' ',firstName, ', ', jobTitle) AS 'Employee Description'
+from employees;
+
+SELECT REPLACE(productLine, 'Cars', 'Automobiles') AS productLine
+FROM products;
+
+SELECT  requiredDate, shippedDate
+FROM orders
+WHERE MONTH(shippedDate) < MONTH(requiredDate) OR 
+      (MONTH(shippedDate) = MONTH(requiredDate) AND DAY(shippedDate) < DAY(requiredDate))
+      and shippedDate IS NOT NULL
+ORDER BY DATEDIFF(day(shippedDate), DAY(requiredDate)) ASC	
+LIMIT 5;
+
+SELECT * 
+FROM orders
+WHERE EXTRACT(YEAR FROM orderDate) = 2005 and month(orderDate) = 5
+		AND ShippedDate IS NULL;
 
 
 
